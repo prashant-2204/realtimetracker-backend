ef3ea15803a8583a0f6bb11159223bc8349a68fc
Prashant <97339424+prashant-2204@users.noreply.github.com>
2024-07-10 19:15:28 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,107 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const socket_io_1 = require("socket.io");
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT || 3001;
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.get('/', (req, res) => {
+    res.send('Welcome to LocShare!');
+});
+const server = app.listen(port, () => {
+    console.log(`Server is running port: ${port}`);
+});
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: '*',
+    },
+});
+const roomCreator = new Map(); // roomid => socketid
+io.on('connection', (socket) => {
+    console.log(`User connected: ${socket.id}`);
+    socket.on('createRoom', (data) => {
+        const roomId = Math.random().toString(36).substring(2, 7);
+        socket.join(roomId);
+        const totalRoomUsers = io.sockets.adapter.rooms.get(roomId);
+        socket.emit('roomCreated', {
+            roomId,
+            position: data.position,
+            totalConnectedUsers: Array.from(totalRoomUsers || []),
+        });
+        roomCreator.set(roomId, socket.id);
+        socket.roomId = roomId; //  attach roomId to socket
+    });
+    socket.on('joinRoom', (data) => {
+        // check if room exists
+        const roomExists = io.sockets.adapter.rooms.has(data.roomId);
+        if (roomExists) {
+            socket.join(data.roomId);
+            socket.roomId = data.roomId; //  attach roomId to socket
+            // Notify the room creator about the new user     
+            const creatorSocketID = roomCreator.get(data.roomId);
+            if (creatorSocketID) {
+                const creatorSocket = io.sockets.sockets.get(creatorSocketID); // get socket instance of creator
+                if (creatorSocket) {
+                    const totalRoomUsers = io.sockets.adapter.rooms.get(data.roomId);
+                    creatorSocket.emit('userJoinedRoom', {
+                        userId: socket.id,
+                        totalConnectedUsers: Array.from(totalRoomUsers || [])
+                    });
+                }
+            }
+            // msg to joiner
+            io.to(`${socket.id}`).emit('roomJoined', {
+                status: 'OK',
+            });
+        }
+        else {
+            io.to(`${socket.id}`).emit('roomJoined', {
+                status: 'ERROR'
+            });
+        }
+    });
+    socket.on('updateLocation', (data) => {
+        io.emit('updateLocationResponse', data);
+    });
+    socket.on('disconnect', () => {
+        console.log(`User disconnected: ${socket.id}`);
+        const roomId = socket.roomId;
+        if (roomId) {
+            // if disconnected user is creator, destroy room
+            if (roomCreator.get(roomId) === socket.id) {
+                // notify users in room that room is destroyed
+                const roomUsers = io.sockets.adapter.rooms.get(roomId);
+                if (roomUsers) {
+                    for (const socketId of roomUsers) {
+                        io.to(`${socketId}`).emit('roomDestroyed', {
+                            status: 'OK'
+                        });
+                    }
+                }
+                io.sockets.adapter.rooms.delete(roomId);
+                roomCreator.delete(roomId);
+            }
+            else {
+                socket.leave(roomId);
+                // notify creator that user left room
+                const creatorSocketId = roomCreator.get(roomId);
+                if (creatorSocketId) {
+                    const creatorSocket = io.sockets.sockets.get(creatorSocketId);
+                    if (creatorSocket) {
+                        creatorSocket.emit('userLeftRoom', {
+                            userId: socket.id,
+                            totalConnectedUsers: Array.from(io.sockets.adapter.rooms.get(roomId) || [])
+                        });
+                    }
+                }
+            }
+        }
+    });
+});
